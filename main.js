58bd422f0d33660aa945c9e6f3ef9e994e6e3793
hsm623 <36944029+hsm623@users.noreply.github.com>
2024-05-10 19:08:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function main() {
   const password = response.data.password;
 
   console.log(username, password);
-  let pikPak = new PikPak(username, password, null, "36972957");
+  let pikPak = new PikPak(username, password, null, "369729571");
 
   pikPak
     .authVerification()
